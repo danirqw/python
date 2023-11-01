ea7fd859c256b11f6841f639e828404a9eb41d51
danirqw <147627054+danirqw@users.noreply.github.com>
2023-11-01 18:47:34 +0300

    dz competed

---
+++ homework.py
@@ -0,0 +1,29 @@
+# number = 1
+# while number < 10:
+#     print("you are welcome")
+#     number += 1
+
+# number = int(input("введите число:"))
+# word = "silence is golden"
+# while number >= 1:
+#     print(word)
+#     number -= 1
+# n = int(input(" введите число:"))
+# while n >=1:
+#     print("0"*5)
+#     n -= 1
+# counter = 0
+# last = 26
+# while counter < 26:
+#     counter += 1
+#     if counter % 2 ==1:
+#         print("первый")
+#     else:
+#         print("второй")
+#     if counter == last:
+#         print("расчёт окончен")    
+#         break
+
+
+
+    
