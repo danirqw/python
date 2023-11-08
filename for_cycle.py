5780be6c5f0ecd8513fae7e884387e6d98599105
danirqw <147627054+danirqw@users.noreply.github.com>
2023-11-08 19:25:55 +0300

    изучили новый цикл

---
+++ for_cycle.py
@@ -0,0 +1,13 @@
+# for i in range(10):
+#     print("какой чудесный день")
+# for i in range(10, 51, 10):
+#     print(i)
+# for letter in "andromeda":
+#     print(letter)
+# shop_list  = ["помидор", "копченное мясо", "салат","масло", "сухари"]
+# for ing in shop_list:
+#     print(f"для салат цезаря нужно {ing}")
+mylist = ["dz", "sport", "hobby"]    
+for do in mylist:
+    print(f"мои дела {do}")
+        
