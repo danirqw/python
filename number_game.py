6a01fd4ca61d53a9cb6b2a1e6525e1d0f718fa72
danirqw <147627054+danirqw@users.noreply.github.com>
2023-11-08 18:45:59 +0300

    доделал игру

--- number_game.py
+++ number_game.py
@@ -2,18 +2,30 @@ import random
 print("Компьютер загадал число от 1 до 100. Попробуй его угадать!")
 secret_number = random.randint(1, 100)
 tries = 0
+name = input("введите имя:").capitalize
 while tries <= 10:
     tries +=1
     print(f"попытка №{tries}")
-    person_number = int(input("Введите число:") )
+    person_number = input("Введите число:") 
+    if not person_number.isdigit():
+        print("неправильный ввод. вводите только числа")
+        tries -=1
+        continue
+    person_number = int(person_number)
     if person_number > secret_number:
-        print("вы вели число больше чем моё")
+        print(f"{name}, вы вели число больше чем моё")
     elif person_number < secret_number:
-        print("вы вели число меньше чем моё")    
+        print(f"{name},вы вели число меньше чем моё")    
     elif person_number == secret_number:
-        print("ты угадал, молодец") 
+        print(f"{name}, ты угадал, молодец") 
         break
     print()
+    if tries ==10:
+        print(f"вы не угадали, число было {secret_number}")
+        
+        
+        
+
        
     
 
